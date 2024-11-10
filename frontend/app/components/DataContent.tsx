206a6bf22b05a7d5cbcb8cc3645cae1eb3019bb5
Nisrayani13 <nisrayani.13@gmail.com>
2024-11-10 17:11:02 +0530

    done with data tab

--- frontend/app/components/DataContent.tsx
+++ frontend/app/components/DataContent.tsx
@@ -1,15 +1,49 @@
+import React, { useRef, useState } from "react";
 import DropdownMenu from "./DropdownMenu";
 
 export default function DataContent(){
+    const [chosenFile,setChosenFile]=useState<File | null>(null);
+    const fileInputRef=useRef(null);
+
+    const uploadFile=()=>{
+        //@ts-ignore
+        fileInputRef.current.click();
+    }
+    const handleFileChange=(event:React.ChangeEvent<HTMLInputElement>)=>{
+        const file = event.target.files ? event.target.files[0] : null;
+        setChosenFile(file);
+    }
+
     return <div className="border rounded-md py-4 px-4">
         <div className="font-semibold text-2xl mb-1">Data Connection</div>
         <div className="text-gray-400 text-sm mb-4">Connect your data source for email customization</div>
-        <button className="flex items-center border rounded-md w-fit p-1.5 gap-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-            </svg>
-            <div>Upload CSV</div>
-        </button>
+        <div className="flex items-center gap-x-4">
+            <input 
+                ref={fileInputRef} 
+                type="file" 
+                className="hidden"
+                onChange={handleFileChange}
+                accept=".csv"
+            ></input>
+            <button 
+            className="flex items-center border rounded-md w-fit p-1.5 gap-x-2"
+            onClick={uploadFile}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                </svg>
+                <div>Upload CSV</div>
+            </button>
+            {chosenFile?(
+                <div className="flex gap-x-1 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+
+                    <div className="text-gray-600 text-base">{chosenFile.name} </div>
+                </div>
+            ):null}
+        </div>
         <div className="mt-7 mb-4">
             <DropdownMenu></DropdownMenu>
         </div>
--- frontend/app/components/DropdownMenu.tsx
+++ frontend/app/components/DropdownMenu.tsx
@@ -2,38 +2,44 @@
 import React, { useState } from 'react'
 
 export default function DropdownMenu() {
+    const options=['Company','Email','Location','Products'];
+
     const [selectedValue,setSelectedValue] =useState("");
     const [toggle,setToggle]=useState(false);
 
-    const handleClick=()=>{
+    const handleDropdown=()=>{
         console.log("clicked")
         setToggle((toggle)=>!toggle);
         console.log(toggle)
     }
 
-    return <div>
-    <div className='mb-1'>Detected Columns</div>
-    <button 
-        className='border rounded-md flex justify-between items-center w-full py-1 px-1.5'
-    >
-        <div className='px-2'>{selectedValue?
-            <span className=''>selectedValue</span>
-            :<span className='text-gray-400'>Select Columns</span>}</div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-        </svg>
-    </button>
+    const handleSelection=(option:string)=>{
+        setSelectedValue((selectedValue)=>option);
+        setToggle((toggle)=>!toggle)
+    }
 
-    <button  onClick={handleClick}>Hi there</button>
-    <h1>MEEEEEEEEEe</h1>
+    return <div className='relative'>
+        <div className='mb-1'>Detected Columns</div>
+        <button 
+            className='border rounded-md flex justify-between items-center w-full py-1.5 px-1.5'
+            onClick={handleDropdown}
+        >
+            <div className='px-2'>{selectedValue?
+                <span className=''>{selectedValue} </span>
+                :<span className='text-gray-400'>Select Columns</span>}</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+        </button>
 
-    {toggle?(
-        <ul className='absolute top-full left-0 w-full mt-1 bg-white rounded-md shadow-sm'>
-            <li>Company</li>
-            <li>Email</li>
-            <li>Location</li>
-            <li>Products</li>
-        </ul>
-    ):null}
+        {toggle ? (
+            <ul className='absolute z-50 overflow-auto w-full mt-1 left-0 right-0 bg-white border rounded-md shadow-lg'>
+                {options.map((option)=>{
+                    return <li className='hover:bg-gray-100 p-1.5 m-1' onClick={()=>handleSelection(option)}>
+                        {option}
+                    </li>
+                })}
+            </ul>
+        ) : null}
     </div>
 }
